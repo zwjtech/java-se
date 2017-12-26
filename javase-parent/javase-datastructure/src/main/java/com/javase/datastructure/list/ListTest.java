e1b01e8c5bcb5eaa8888e6dc0d1d51ea7836aa04
zhangweijie <zhangweijie@jiagouyun.com>
2017-12-26 11:19:45 +0800

    17-12-26

--- javase-parent/javase-datastructure/src/main/java/com/javase/datastructure/list/ListTest.java
+++ javase-parent/javase-datastructure/src/main/java/com/javase/datastructure/list/ListTest.java
@@ -13,11 +13,22 @@ public class ListTest {
     }
 
     @Test
-    public void testRemove(){
+    public void testRemove1(){
         List<Person> personList=new ArrayList<Person>();
         personList.add(new Person("zwj1","张1"));
         personList.add(new Person("zwj2","张2"));
         Boolean b=personList.remove(new Person("zwj1","张1"));//没有移除成功o
         System.out.println(personList);
+    }
+
+    @Test
+    public void testRemove2(){
+        List<Person> personList=new ArrayList<Person>();
+        Person p1=new Person("zwj1","张1");
+        personList.add(p1);
+        Person p2=new Person("zwj2","张2");
+        personList.add(p2);
+        Boolean b=personList.remove(p1);//移除成功o
+        System.out.println(b+","+personList);
     }
 }
